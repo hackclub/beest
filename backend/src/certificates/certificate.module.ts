f1ad52854b0415042f69c3f1c7aa2e6187663070
EDRipper <edripper@users.noreply.github.com>
2026-09-16 17:24:28 +0000

    Revert "Add AuthModule to CertificateModule imports"
    This reverts commit 2762309df54bdfcf1802dfb000705bb4c64f6f08.

--- backend/src/certificates/certificate.module.ts
+++ backend/src/certificates/certificate.module.ts
@@ -6,12 +6,11 @@ import { User } from '../entities/user.entity';
 import { CertificateService } from './certificate.service';
 import { CertificateController } from './certificate.controller';
 import { AuditLogModule } from '../audit-log/audit-log.module';
-import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Certificate, Order, User]),
-    AuditLogModule,AuthModule,
+    AuditLogModule,
   ],
   controllers: [CertificateController],
   providers: [CertificateService],
